3d04ef029fea4f8c10226abb9568116bba679ea3
Chandrika Guntur <cguntur@gcsdesign.com>
2018-08-14 20:56:46 -0700

    Changed the header and footer menu, added a new template for the new home page and added two new pages - PLI and PFP

--- functions.php
+++ functions.php
@@ -1,20 +1,25 @@
 <?php
 
 // Featured Image Support
-// add_theme_support( 'post-thumbnails' );
+ //add_theme_support( 'post-thumbnails' );
 
 // Enqueueing Scripts & Styles
 function include_scripts() {
 
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
 
-	wp_enqueue_script('isotope', 'https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js', array('jquery'), null, true);
+  wp_enqueue_script('isotope', 'https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js', array('jquery'), null, true);
 
 	wp_enqueue_script('bootstrap', get_template_directory_uri() .'/assets/js/bootstrap.min.js', array('jquery'), null, true);
 
 	wp_enqueue_script('main-scripts', get_template_directory_uri() .'/assets/js/main.js', array('jquery'), null, true);
-
 }
 add_action('wp_enqueue_scripts', 'include_scripts');
+
+function peaxy_register_main_menu() {
+  register_nav_menu('main-menu',__( 'Main Menu' ));
+	register_nav_menu('footer-menu', __('Footer Menu'));
+}
+add_action( 'init', 'peaxy_register_main_menu' );
 
 ?>
--- header.php
+++ header.php
@@ -9,15 +9,24 @@
 
     <?php wp_head();?>
 </head>
-<body>
+<body <?php body_class(); ?>>
 
-    <!-- HEADER test -->	
+    <!-- HEADER -->
     <div class="main-header">
         <a class="peaxy-logo" href="/">
             <img src="<?php bloginfo('template_directory'); ?>/assets/img/logo.png" alt="Peaxy Logo">
         </a>
         <a class="mobile-menu" href="javascript:void(0);">MENU</a>
-        <ul class="nav" id="main-navigation">
+        <?php //wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+        <?php
+        wp_nav_menu( array(
+          'theme_location' => 'main-menu',
+          'container'      => 'ul',
+          'menu_class'     => 'mainmenu pull-left nav',
+          'menu_id'        => 'main-navigation'
+        ) );
+        ?>
+        <!--<ul class="nav" id="main-navigation">
             <li>
                 <a href="/digital-twin">Digital Twin</a>
             </li>
@@ -35,7 +44,7 @@
             </li>
             <li>
                 <a href="/blog/">Blog</a>
-            </li>
+            </li>-->
             <!-- <li>
                 <form action="https://www.google.com/search" role="search" method="get" class="search-form">
                     <div class="field-wrapper">
@@ -43,6 +52,6 @@
                     </div>
                     <input type="hidden" name="sitesearch" value="peaxy.net">
                 </form>
-            </li> -->
-        </ul>
+            </li>
+        </ul>-->
     </div>
